ab3c6f15ad3336b7c8031e993f4f0548cbdbef24
Shreeram Kelkar <shreeram.kelkar@ril.com>
2023-10-03 12:56:33 +0530

    Initial Commit

---
+++ OTTTemplateApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  OTTTemplateApp
+//
+//  Created by Shreeram Kelkar on 03/10/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
